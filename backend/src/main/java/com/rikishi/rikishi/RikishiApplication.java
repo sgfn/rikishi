c8e63c1bae03c7a1a5b0f82798e06d234735ddb2
Jakub Pisarek <99591440+sgfn@users.noreply.github.com>
2024-05-08 01:49:07 +0200

    Setup FE and BE linters

--- backend/src/main/java/com/rikishi/rikishi/RikishiApplication.java
+++ backend/src/main/java/com/rikishi/rikishi/RikishiApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RikishiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RikishiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(RikishiApplication.class, args);
+    }
 }
