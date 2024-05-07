8eb725e51dcb90daa6b90ad638ad6339faf5ad0a
c-i-a-s-t-e-k <jezowdominik@duck.com>
2024-05-07 21:30:03 +0200

    tree bracket functionality to review

--- backend/app/src/main/java/backend/matching/systems/MatchingSystem.java
+++ backend/app/src/main/java/backend/matching/systems/MatchingSystem.java
@@ -4,6 +4,7 @@ import backend.matching.Player;
 import org.graalvm.collections.Pair;
 
 import java.util.Collection;
+import java.util.Set;
 
 public interface MatchingSystem {
 //    run in structure to the next sumo confrontation
@@ -11,7 +12,7 @@ public interface MatchingSystem {
 //    allow to choose winner of match
     public void chooseWinner(Player winner);
 //    return who will fight/ is fighting now
-    public Pair<Player, Player> getCurrentPlayers();
+    public Set<Player> getCurrentPlayers();
 //    allow to upload all players who will fight on contest
     public void loadPlayers(Collection<Player> players);
 }
