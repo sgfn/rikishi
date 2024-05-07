b9a2787fc80cf6bb6bf97a618eeab49cdada0a15
Jakub Pisarek <99591440+sgfn@users.noreply.github.com>
2024-05-08 01:24:40 +0200

    Replace `backend` with spring boot template

---
+++ backend/src/main/java/com/rikishi/rikishi/RikishiApplication.java
@@ -0,0 +1,13 @@
+package com.rikishi.rikishi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RikishiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RikishiApplication.class, args);
+	}
+
+}
