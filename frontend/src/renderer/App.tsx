873c8688fa5f15ad29431dd98264ffa71b64f762
Zosia <zburchard@student.agh.edu.pl>
2024-05-11 18:16:21 +0200

    done

--- frontend/src/renderer/App.tsx
+++ frontend/src/renderer/App.tsx
@@ -1,10 +1,11 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import React, { useEffect } from 'react';
+import React, { useEffect} from 'react';
 import axios from 'axios';
 import config from '../config.js';
 import './App.css';
 import ContestantsList from '../components/ContestantsList';
 import ContestantProfile from '../components/ContestantProfile';
+import MainView from '../components/MainView.js';
 
 export default function App() {
   // TODO: this healthcheck should be called periodically
@@ -21,12 +22,15 @@ export default function App() {
   }, []);
 
   return (
-    <Router>
-      <Routes>
-      	{/*TODO: main page component to root '/' path */}
-        <Route exact path="/contestants" element={<ContestantsList />} />
-        <Route exact path="/contestants/:id" element={<ContestantProfile />} />
-      </Routes>
-    </Router>
+    <div>
+      <Router>
+        <Routes>
+          {/* TODO: main page component to root '/' path */}
+          <Route exact path="/" element={<MainView />} />
+          <Route exact path="/contestants" element={<ContestantsList />} />
+          <Route exact path="/contestants/:id" element={<ContestantProfile />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
