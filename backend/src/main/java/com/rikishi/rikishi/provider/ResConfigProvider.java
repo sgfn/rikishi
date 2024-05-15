798bbcb4b07bf22c5cd63e129d1ab34ca59f2f91
Daniel <dbcongard@gmail.com>
2024-05-15 22:10:51 +0200

    Add JSON config file support and place weight classes into `resources/config.json`

---
+++ backend/src/main/java/com/rikishi/rikishi/provider/ResConfigProvider.java
@@ -0,0 +1,38 @@
+package com.rikishi.rikishi.provider;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rikishi.rikishi.model.WeightClass;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * ConfigProvider that loads data from the resources.
+ * Data was taken from <a href="http://www.ifs-sumo.org/ifs-weight-class.html">ifs-sumo.org</a>
+ */
+@Component
+public class ResConfigProvider implements ConfigProvider {
+    private final Config config;
+
+    public ResConfigProvider(ObjectMapper mapper) throws IOException {
+        config = mapper.readValue(getClass().getResource("/config.json"), Config.class);
+    }
+
+    @Override
+    public List<WeightClass> getWeightClasses() {
+        return config.weightClasses;
+    }
+
+    @Override
+    public Optional<WeightClass> getWeightClassById(long id) {
+        return config.weightClasses.stream()
+            .filter(weightClass -> weightClass.id() == id)
+            .findFirst();
+    }
+
+    private record Config(
+        List<WeightClass> weightClasses
+    ) {}
+}
