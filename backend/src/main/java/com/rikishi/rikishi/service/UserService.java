74e7dc55abacd899d4308454a6b1438a3ee989a5
Daniel <dbcongard@gmail.com>
2024-05-20 22:02:15 +0200

    Add `UserRepository` and use it in `UserService`

--- backend/src/main/java/com/rikishi/rikishi/service/UserService.java
+++ backend/src/main/java/com/rikishi/rikishi/service/UserService.java
@@ -2,9 +2,10 @@ package com.rikishi.rikishi.service;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
+import com.rikishi.rikishi.loader.UserLoader;
+import com.rikishi.rikishi.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import com.rikishi.rikishi.model.User;
@@ -12,16 +13,23 @@ import com.rikishi.rikishi.model.User;
 @Service("us")
 public class UserService {
     private static Map<Long, User> users = new HashMap<>();
+    private final UserRepository userRepository;
+    private final UserLoader userLoader;
+
+    public UserService(UserRepository userRepository, UserLoader userLoader) throws IOException {
+        this.userRepository = userRepository;
+        this.userLoader = userLoader;
+    }
 
     public void addUser(User user) {
-        users.put(user.id(), user);
+        userRepository.add(user);
     }
 
     public List<User> getUsers() {
-        return users.values().stream().toList();
+        return userRepository.findAll().toList();
     }
 
     public Optional<User> getUserById(long id) {
-        return users.containsKey(id) ? Optional.of(users.get(id)) : Optional.empty();
+        return userRepository.findById(id);
     }
 }
