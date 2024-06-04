1ea000c6ea97ddec2aa04a00d3d6f54efc3947c6
adam147g <adasadas223@wp.pl>
2024-06-04 12:58:23 +0200

    swapPlayers with tests

--- backend/src/test/java/com/rikishi/backend/MatchingSystemTest.java
+++ backend/src/test/java/com/rikishi/backend/MatchingSystemTest.java
@@ -5,9 +5,10 @@ import com.rikishi.backend.matching.systems.MatchingSystem;
 import com.rikishi.backend.matching.systems.TreeBracket;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class MatchingSystemTest {
     @Test
@@ -62,5 +63,35 @@ class MatchingSystemTest {
         }
         Set<Player> currPlayers = bracket.getCurrentPlayers();
         bracket.chooseWinner(currPlayers.iterator().next());
+    }
+
+    @Test
+    void swapPlayersTest() {
+        List<Player> players = List.of(
+            new Player(), new Player(), new Player(), new Player(),
+            new Player(), new Player(), new Player(), new Player()
+        );
+        TreeBracket bracket = new TreeBracket();
+        bracket.loadPlayers(players);
+
+        System.out.println("Bracket before swapping:");
+        bracket.printBracket();
+
+        var player1 = bracket.getArrayTree().get(14 - 1);
+        var player6 = bracket.getArrayTree().get(14 - 6);
+
+        // Swapping players at indices 7 and 8 (leaf nodes)
+        bracket.swapPlayers(1, 6);
+
+        System.out.println("\nBracket after swapping:");
+        bracket.printBracket();
+
+        // Verify the swap
+        Assertions.assertEquals(player6, bracket.getArrayTree().get(14 - 1));
+        Assertions.assertEquals(player1, bracket.getArrayTree().get(14 - 6));
+
+        assertThrows(IllegalArgumentException.class, () -> bracket.swapPlayers(9, 3));
+        assertThrows(IllegalArgumentException.class, () -> bracket.swapPlayers(3, 19));
+
     }
 }
