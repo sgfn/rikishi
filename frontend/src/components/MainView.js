823cc8ca79b7029701b447c1455ac095af5e45c8
Zosia <zburchard@student.agh.edu.pl>
2024-06-07 22:14:36 +0200

    dodanie przycisku wgrywania pliku

--- frontend/src/components/MainView.js
+++ frontend/src/components/MainView.js
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import './MainView.css';
 import startPhoto from '../../assets/sumuStart.jpg';
+import { useState } from 'react';
 
 function MainView() {
   const navigator = useNavigate();
@@ -14,6 +15,47 @@ function MainView() {
   const handleRaport = () => {
     navigator('/ladder');
   };
+
+  const [file, setFile] = useState(null);
+  const [uploadStatus, setUploadStatus] = useState(null);
+
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleUpload = () => {
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = async (event) => {
+        const csvContent = event.target.result;
+
+        const requestBody = {
+          path: csvContent,
+        };
+
+        try {
+          const response = await fetch('/contestants/import-csv', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(requestBody),
+          });
+
+          if (response.ok) {
+            setUploadStatus('File uploaded successfully');
+          } else {
+            setUploadStatus('Failed to upload file');
+          }
+        } catch (error) {
+          setUploadStatus('Error uploading file');
+          console.error('Error uploading file:', error);
+        }
+      };
+      reader.readAsText(file);
+    }
+  };
+
   return (
     <div className="main-view">
       <h2>Welcome to the sumo competition!</h2>
@@ -27,6 +69,18 @@ function MainView() {
         </button>
         <button onClick={handleRaport} type="button" className="goToList">
           Go to Ladder tournament
+        </button>
+      </div>
+      <div className="upload-container">
+        <input
+          className="file-input"
+          type="file"
+          accept=".csv"
+          onChange={handleFileChange}
+        />
+        <button onClick={handleUpload} type="button">
+          Upload CSV
+          {uploadStatus && <p>{uploadStatus}</p>}
         </button>
       </div>
     </div>
